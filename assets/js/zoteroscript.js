f27f9392eeca806a0286296bad1c8fbeab82560b
Inga Schöyen <93376973+ingaschoeyen@users.noreply.github.com>
2023-05-16 18:04:36 +0200

    Create zoteroscript.js

---
+++ assets/js/zoteroscript.js
@@ -0,0 +1,9 @@
+// script to load references and descriptions to literature page
+
+// function to load references
+async function load_references(){
+  
+  
+}
+
+// function to create the container and add in the reference formatted
